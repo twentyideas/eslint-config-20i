f58cce6a01543e5a20bbd8b3f112afa814c11793
Greg McKelvey <mckelveygreg@gmail.com>
2023-10-17 11:39:53 -0700

    feat: :boom: major updates for eslint, prettier, and ts
    There are new ts rules and adjusted prettier settings. Our large monorepo faired well, only cosmetic updates needed
    
    update eslint, prettier, and ts to latest versions!

--- react.js
+++ react.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   extends: [
     "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
     "./.eslintrc.js",
